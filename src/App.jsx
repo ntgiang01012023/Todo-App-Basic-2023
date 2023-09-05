f184c0a1fac4a5a4a9202f36cafeb183caf24a32
ntgiang01012023 <ntgiang01012023@gmail.com>
2023-09-05 10:11:44 +0700

    Done App

--- src/App.jsx
+++ src/App.jsx
@@ -34,8 +34,8 @@ export default function App() {
         }
     }, []);
 
-  return (
-    <div className='w-[400px] h-[500px] bg-slate-200 rounded-[5px] flex flex-col justify-start items-center gap-4 p-[24px]'>
+    return (
+        <div className='w-[400px] h-[500px] bg-slate-200 rounded-[5px] flex flex-col justify-start items-center gap-4 p-[24px]'>
             <h1 className='text-[24px] font-bold'>TODO APP</h1>
             <div className="flex justify-between items-center w-full">
                 <input
@@ -52,21 +52,20 @@ export default function App() {
                     Add Task
                 </button>
             </div>
-                <ul className='w-full h-auto'>
-                    {todos.map((task, index) => (
-                        <li 
-                            className='w-full h-[35px] flex justify-between items-center bg-white rounded-tl-[5px] rounded-bl-[5px] outline-none mb-2 pl-2'
-                            key={index}>
-                            {task}
-                            <button 
-                                className='w-[100px] h-full bg-green-200 rounded-tr-[5px] rounded-br-[5px]'
-                                onClick={() => handleDeleteTask(index)}>
-                                    Delete
-                            </button>
-                        </li>
-                    ))}
-                </ul>
-        
-    </div>
-  )
+            <ul className='w-full h-auto'>
+                {todos.map((task, index) => (
+                    <li 
+                        className='w-full h-[35px] flex justify-between items-center bg-white rounded-tl-[5px] rounded-bl-[5px] outline-none mb-2 pl-2'
+                        key={index}>
+                        {task}
+                        <button 
+                            className='w-[100px] h-full bg-green-200 rounded-tr-[5px] rounded-br-[5px]'
+                            onClick={() => handleDeleteTask(index)}>
+                                Delete
+                        </button>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    )
 }
